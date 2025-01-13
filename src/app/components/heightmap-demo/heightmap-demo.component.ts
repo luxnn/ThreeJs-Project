837c3d18e60aa7f2f465298b897a4c6eda147b64
Lukas Zöhrer <lukas.zoehrer@stud.it.htl-hl.ac.at>
2025-01-13 08:11:06 +0100

    normalmap geht nicht

--- src/app/components/heightmap-demo/heightmap-demo.component.ts
+++ src/app/components/heightmap-demo/heightmap-demo.component.ts
@@ -48,26 +48,7 @@ export class HeightmapDemoComponent
   ngOnInit() {}
 
   ngAfterViewInit(): void {
-    this.initializeScene();
-    this.initializeRenderer();
-    this.initializeCamera();
-    this.addLighting();
-    this.addEventListeners();
-    this.loadTextures();
-    this.animate();
-  }
-
-  private initializeScene() {
     this.scene = new Scene();
-  }
-
-  private initializeRenderer() {
-    this.renderer = new WebGLRenderer({ canvas: this.canvasRef.nativeElement });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.shadowMap.enabled = true;
-  }
-
-  private initializeCamera() {
     this.camera = new PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
@@ -76,25 +57,25 @@ export class HeightmapDemoComponent
     );
     this.camera.position.set(0, 25, 25);
     this.camera.lookAt(0, 0, 0);
-  }
 
-  private addLighting() {
-    const directionalLight = new PointLight(0xffffff, 0.5);
-    directionalLight.distance = 25;
-    directionalLight.power = 1000;
-    directionalLight.position.set(0, 8, 0);
-    directionalLight.castShadow = true;
-    directionalLight.shadow.mapSize.width = 2048;
-    directionalLight.shadow.mapSize.height = 2048;
-    this.scene.add(directionalLight);
+    this.renderer = new WebGLRenderer({ canvas: this.canvasRef.nativeElement });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.shadowMap.enabled = true;
 
-    const ambientLight = new AmbientLight(0x403040, 1);
-    this.scene.add(ambientLight);
-  }
+    const loader = new TextureLoader();
+    loader.load('assets/heightmap.png', (texture: Texture) =>
+      this.onTextureLoaded(texture)
+    );
 
-  private addEventListeners() {
+    // Lade auch die normalmap als Bodentextur
+    loader.load('assets/normalmap.png', (normalMap: Texture) =>
+      this.createFloor(normalMap)
+    );
+
     window.addEventListener('resize', this.onWindowResize.bind(this));
     this.addMouseListeners();
+
+    this.animate();
   }
 
   private addMouseListeners() {
@@ -133,18 +114,6 @@ export class HeightmapDemoComponent
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  private loadTextures() {
-    const loader = new TextureLoader();
-
-    loader.load('assets/heightmap.png', (texture: Texture) =>
-      this.onTextureLoaded(texture)
-    );
-
-    loader.load('assets/normalmap.png', (normalMap: Texture) =>
-      this.createFloor(normalMap)
-    );
-  }
-
   private onTextureLoaded(texture: Texture) {
     const canvas = document.createElement('canvas');
     canvas.width = texture.image.width;
@@ -155,9 +124,16 @@ export class HeightmapDemoComponent
 
     const data = context.getImageData(0, 0, canvas.width, canvas.height);
     this.generateTerrain(data);
+    this.addCube(-5, 4, -5, 0xff0000); // Rot
+    this.addSphere(0, 3, -5, 0x0000ff); // Blau
+    this.generateLight();
+
+    // Füge eine Geometrie mit Phong Material hinzu
+    this.addPhongSphere(5, 3, 0, 0x00ff00); // Grünes Phong-Sphere
   }
 
   private createFloor(normalMap: Texture) {
+    // PlaneGeometry für den Boden
     const geometry = new PlaneGeometry(500, 500, 50, 50);
     const material = new MeshStandardMaterial({
       color: 0x888888,
@@ -167,48 +143,178 @@ export class HeightmapDemoComponent
     });
 
     const floor = new Mesh(geometry, material);
-    floor.rotation.x = -Math.PI / 2;
+    floor.rotation.x = -Math.PI / 2; // Drehe den Boden, um ihn horizontal zu platzieren
     floor.receiveShadow = true;
     this.scene.add(floor);
   }
 
+  private addCube(x: number, y: number, z: number, color: number) {
+    const material = new MeshStandardMaterial({ color: color });
+    const box = new BoxGeometry(3, 3, 3);
+    const mesh = new Mesh(box, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.position.set(x, y, z);
+
+    // Würfelbewegung initialisieren
+    mesh.name = 'MovingCube'; // Name setzen, um ihn später zu finden
+    mesh.userData['direction'] = 1; // 1 für rechts, -1 für links
+
+    this.scene.add(mesh);
+  }
+
+  private addSphere(x: number, y: number, z: number, color: number) {
+    const material = new MeshStandardMaterial({ color: color });
+    const sphere = new SphereGeometry(2, 32, 32);
+    const mesh = new Mesh(sphere, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.position.set(x, y, z);
+    this.scene.add(mesh);
+  }
+
+  private addPhongSphere(x: number, y: number, z: number, color: number) {
+    // Geometrie und Phong-Material hinzufügen
+    const geometry = new SphereGeometry(3, 32, 32);
+    const material = new MeshStandardMaterial({
+      color: color,
+    });
+
+    const sphere = new Mesh(geometry, material);
+    sphere.position.set(x, y, z);
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
+
+    this.scene.add(sphere);
+  }
+
   private generateTerrain(imageData: ImageData) {
     const vertices = [];
-    const indices: number[] = [];
+    const colorInfos = [
+      [0.53, 0.71, 0.42],
+      [0.64, 0.77, 0.53],
+      [0.74, 0.82, 0.63],
+      [0.85, 0.87, 0.74],
+      [0.94, 0.91, 0.8],
+      [0.97, 0.85, 0.64],
+      [0.91, 0.76, 0.51],
+      [0.79, 0.63, 0.43],
+      [0.68, 0.53, 0.34],
+      [0.5, 0.38, 0.23],
+    ];
+    const colors = [];
 
-    for (let z = 0; z < imageData.height - 1; z++) {
-      const offset = z * imageData.width;
-      for (let x = 0; x < imageData.width - 1; x++) {
-        indices.push(
-          offset + x,
-          offset + x + imageData.width,
-          offset + x + 1,
-          offset + x + 1,
-          offset + x + imageData.width,
-          offset + x + imageData.width + 1
-        );
+    for (let z = 0; z < imageData.height; z++) {
+      for (let x = 0; x < imageData.width; x++) {
+        const index = x * 4 + z * imageData.width * 4;
+        const y = imageData.data[index] / 255;
+
+        vertices.push(x - imageData.width / 2, y * 5, z - imageData.height / 2);
+
+        const colorIndex = Math.min(Math.max(Math.floor(y * 10), 0), 9);
+        colors.push(...colorInfos[colorIndex], 1);
+      }
+    }
+
+    const indices: number[] = [];
+    for (let j = 0; j < imageData.height - 1; j++) {
+      let offset = j * imageData.width;
+      for (let i = offset; i < offset + imageData.height - 1; i++) {
+        indices.push(i, i + imageData.width, i + 1);
+        indices.push(i + 1, i + imageData.width, i + 1 + imageData.width);
       }
     }
 
     const geometry = new BufferGeometry();
     geometry.setIndex(indices);
-    this.map = new Mesh(
-      geometry,
-      new MeshStandardMaterial({
-        color: 0x808080,
-      })
+    geometry.setAttribute(
+      'position',
+      new BufferAttribute(new Float32Array(vertices), 3)
     );
+    geometry.setAttribute(
+      'color',
+      new BufferAttribute(new Float32Array(colors), 4)
+    );
+
+    const material = new MeshStandardMaterial({
+      vertexColors: true,
+      flatShading: true,
+    });
+
+    this.map = new Mesh(geometry, material);
+    this.map.castShadow = true;
+    this.map.receiveShadow = true;
     this.scene.add(this.map);
   }
 
-  private animate() {
-    this.animationFrameId = requestAnimationFrame(() => this.animate());
-    this.renderer.render(this.scene, this.camera);
+  private generateLight() {
+    this.createDirectionalLight();
+    this.addAmbientLight();
+  }
+
+  private createDirectionalLight() {
+    const directionalLight = new PointLight(0xffffff, 0.5);
+    directionalLight.distance = 25;
+    directionalLight.power = 1000;
+    directionalLight.position.set(0, 8, 0);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    directionalLight.shadow.camera.near = 1;
+    directionalLight.shadow.camera.far = 200;
+    this.scene.add(directionalLight);
+  }
+
+  private addAmbientLight() {
+    const ambientLight = new AmbientLight(0x403040, 1);
+    this.scene.add(ambientLight);
   }
 
   ngOnDestroy() {
     cancelAnimationFrame(this.animationFrameId);
     window.removeEventListener('resize', this.onWindowResize.bind(this));
     this.renderer.dispose();
+  }
+
+  private animate() {
+    this.animationFrameId = requestAnimationFrame(() => this.animate());
+
+    if (this.map) {
+      this.map.rotation.y += 0.01; // Terrain dreht sich
+    }
+
+    // Bewegung des Würfels
+    const cube = this.scene.children.find((child) => child.name === 'MovingCube') as Mesh;
+    if (cube) {
+      const speed = 0.05;
+      const maxDistance = 10;
+
+      cube.position.x += speed * cube.userData['direction'];
+      if (cube.position.x > maxDistance || cube.position.x < -maxDistance) {
+        cube.userData['direction'] *= -1; // Richtung umkehren
+      }
+    }
+
+    // Bewegung der Lichtquelle
+    const lightRadius = 20;
+    const lightHeight = 15;
+    const lightSpeed = 0.0005;
+
+    const directionalLight = this.scene.children.find(
+      (child) => child instanceof PointLight
+    ) as PointLight;
+
+    if (directionalLight) {
+      const time = Date.now() * lightSpeed;
+      directionalLight.position.set(
+        lightRadius * Math.cos(time),
+        lightHeight,
+        lightRadius * Math.sin(time)
+      );
+
+      directionalLight.lookAt(0, 0, 0);
+    }
+
+    this.renderer.render(this.scene, this.camera);
   }
 }
